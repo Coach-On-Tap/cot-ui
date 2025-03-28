17d87ea5271918bc8721dd59e6981fbe1b32a9f4
phongdang707 <phongdang707@gmail.com>
2025-03-28 11:34:48 +0700

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -1,33 +1,16 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import path from 'path';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  plugins: [react()],
   build: {
     lib: {
-      entry: path.resolve(__dirname, 'src/index.ts'),
+      entry: 'src/index.ts',
       name: '@coach-on-tap/cot-ui',
-      fileName: (format) => `index.${format}.js`,
-      formats: ['es', 'cjs'],
+      formats: ['es', 'cjs']
     },
     rollupOptions: {
-      external: [
-        'react',
-        'react-dom',
-        '@mui/material',
-        '@emotion/react',
-        '@emotion/styled',
-        '@emotion/server',
-      ],
-      output: {
-        globals: {
-          react: 'React',
-          'react-dom': 'ReactDOM',
-        },
-      },
-    },
-    sourcemap: true,
-    target: 'esnext',
+      external: ['react', 'react-dom']
+    }
   },
-});
+  plugins: [react()]
+})
